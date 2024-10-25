17d31eb04f9fab8898b5e3446d11f3e71b60464e
sadiq1122 <sadiq.977fg@gmail.com>
2024-10-25 10:47:10 -0700

    initial commit

---
+++ src/test/java/com/example/algo101/Algo101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.algo101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Algo101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
